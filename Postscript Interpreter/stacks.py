910ea28ae722d1fc08e541c560c0da8c2aa2851e
Ben Nachmanson <bennachmanson@gmail.com>
2022-01-13 10:13:22 -0800

    Adding Project

---
+++ Postscript Interpreter/stacks.py
@@ -0,0 +1,424 @@
+# Name: Ben Nachmanson
+from psElements import Value, StrConstant, ArrayConstant, FunctionBody
+
+
+class Stacks:
+    def __init__(self):
+        # stack variables
+        # assuming top of the stack is the end of the list
+        self.opstack = []
+        self.dictstack = []  # assuming top of the stack is the end of the list
+
+        # The builtin operators supported by our interpreter
+        self.builtin_operators = {
+            # TO-DO in part1
+            'add': self.add,
+            'sub': self.sub,
+            'mul': self.mul,
+            'eq': self.eq,
+            'lt': self.lt,
+            'gt': self.gt,
+            'length': self.length,
+            'get': self.get,
+            'put': self.put,
+            'pop': self.pop,
+            'stack': self.stack,
+            'dup': self.dup,
+            'copy': self.copy,
+            'count': self.count,
+            'clear': self.clear,
+            'exch': self.exch,
+            'dict': self.psDict,
+            'begin': self.begin,
+            'end': self.end,
+            'def': self.psDef,
+            'ifelse': self.psIfelse,
+            'if': self.psIf,
+            'for': self.psFor
+        }
+
+    # -------  Operand Stack Operators --------------
+    """
+        Helper function. Pops the top value from opstack and returns it.
+    """
+
+    def opPop(self):
+        if len(self.opstack) < 1:
+            print("Error. stack is empty")
+        else:
+            return self.opstack.pop()
+
+    """
+       Helper function. Pushes the given value to the opstack.
+    """
+
+    def opPush(self, value):
+        self.opstack.append(value)
+
+    # ------- Dict Stack Operators --------------
+
+    """
+       Helper function. Pops the top dictionary from dictstack and returns it.
+    """
+
+    def dictPop(self):
+        return self.dictstack.pop()
+
+    """
+       Helper function. Pushes the given dictionary onto the dictstack.
+    """
+
+    def dictPush(self, d):
+        self.dictstack.append(d)
+    """
+       Helper function. Adds name:value pair to the top dictionary in the dictstack.
+       (Note: If the dictstack is empty, first adds an empty dictionary to the dictstack then adds the name:value to that.
+    """
+
+    def define(self, name, value):
+        if len(self.dictstack) < 1:
+            self.dictstack.append({name: value})
+        else:
+            self.dictstack[-1][name] = value
+
+    """
+       Helper function. Searches the dictstack for a variable or function and returns its value.
+       (Starts searching at the top of the opstack; if name is not found returns None and prints an error message.
+        Make sure to add '/' to the begining of the name.)
+    """
+
+    def lookup(self, name):
+
+        i = len(self.dictstack) - 1
+
+        while i >= 0:
+            if "/" + name in self.dictstack[i]:
+                return self.dictstack[i]["/" + name]
+            i -= 1
+        return None
+
+    # ------- Arithmetic Operators --------------
+
+    def add(self):
+        if len(self.opstack) > 1:
+            op1 = self.opPop()
+            op2 = self.opPop()
+            if isinstance(op1, int) and isinstance(op2, int):
+                self.opPush(op1 + op2)
+            else:
+                print("Error: add - one of the operands is not a number value")
+                self.opPush(op2)
+                self.opPush(op1)
+        else:
+            print("Error: add expects 2 operands")
+
+    """
+       Pop 2 values from opstack; checks if they are numerical (int); subtracts them; and pushes the result back to opstack.
+    """
+
+    def sub(self):
+        if len(self.opstack) > 1:
+            op1 = self.opPop()
+            op2 = self.opPop()
+            if isinstance(op1, int) and isinstance(op2, int):
+                self.opPush(op2 - op1)
+            else:
+                print("Error: sub - one of the operands is not a number value")
+                self.opPush(op2)
+                self.opPush(op1)
+        else:
+            print("Error: sub expects 2 operands")
+
+    """
+        Pops 2 values from opstack; checks if they are numerical (int); multiplies them; and pushes the result back to opstack.
+    """
+
+    def mul(self):
+        if len(self.opstack) > 1:
+            op1 = self.opPop()
+            op2 = self.opPop()
+            if isinstance(op1, int) and isinstance(op2, int):
+                self.opPush(op1 * op2)
+            else:
+                print("Error: mul - one of the operands is not a number value")
+                self.opPush(op2)
+                self.opPush(op1)
+        else:
+            print("Error: mul expects 2 operands")
+
+    # ---------- Comparison Operators  -----------------
+    """
+       Pops the top two values from the opstack; pushes "True" is they are equal, otherwise pushes "False"
+    """
+
+    def eq(self):
+        if len(self.opstack) >= 1:
+            op1 = self.opPop()
+            op2 = self.opPop()
+            if op1 == op2:
+                self.opPush(True)
+            else:
+                self.opPush(False)
+        else:
+            print("error: less than two items in the stack.")
+
+    """
+       Pops the top two values from the opstack; pushes "True" if the bottom value is less than the top value, otherwise pushes "False"
+    """
+
+    def lt(self):
+        if(len(self.opstack) > 1):
+            op1 = self.opPop()
+            op2 = self.opPop()
+            if (op2 < op1):
+                self.opPush(True)
+            else:
+                self.opPush(False)
+        else:
+            print("error: not enough items in the list.")
+
+    """
+       Pops the top two values from the opstack; pushes "False" if the bottom value is greater than the top value, otherwise pushes "False"
+    """
+
+    def gt(self):
+        if(len(self.opstack) > 1):
+            op1 = self.opPop()
+            op2 = self.opPop()
+            if(op2 > op1):
+                self.opPush(True)
+            else:
+                self.opPush(False)
+        else:
+            print("error: not enough items in the list. ")
+
+    # ------- String and Array Operators --------------
+    """
+       Pops a string or array value from the operand opstack and calculates the length of it. Pushes the length back onto the opstack.
+       The `length` method should support both ArrayConstant and StrConstant values.
+    """
+
+    def length(self):
+        if len(self.opstack) > 0:
+            ob = self.opPop()
+            if isinstance(ob, StrConstant):
+                self.opPush(len(ob.value)-2)
+            elif isinstance(ob, ArrayConstant):
+                self.opPush(len(ob.value))
+            else:
+                self.opPush(ob.value)
+
+    """
+        Pops a StrConstant or an ArrayConstant and an index from the operand opstack.
+        If the argument is a StrConstant, pushes the ascii value of the the character in the string at the index onto the opstack;
+        If the argument is an ArrayConstant, pushes the value at the `index` of array onto the opstack;
+    """
+
+    def get(self):
+        if len(self.opstack) > 1:
+            index = self.opPop()
+            ob = self.opPop()
+            if(isinstance(ob, StrConstant)):
+                self.opPush(ord(ob.value[index+1]))
+            elif(isinstance(ob, ArrayConstant)):
+                self.opPush(ob.value[index])
+        else:
+            print("error: stack does not have two values")
+
+    """
+    Pops a StrConstant or ArrayConstant value, an (zero-based) `index`, and an `item` from the opstack
+    If the argument is a StrConstant, replaces the character at `index` of the StrConstant's string with the character having the ASCII value of `item`.
+    If the argument is an ArrayConstant, replaces the element at `index` of the ArrayConstant's list with the value `item`.
+    """
+
+    def put(self):
+        if len(self.opstack) > 2:
+            item = self.opPop()
+            index = self.opPop()
+            ob = self.opPop()
+            if(isinstance(ob, StrConstant)):
+                ob.value = ob.value[: index+1] + \
+                    chr(item) + ob.value[index + 2:]
+            elif(isinstance(ob, ArrayConstant)):
+                ob.value[index] = item
+        else:
+            print("error-put: not enough values")
+
+    # ------- Stack Manipulation and Print Operators --------------
+
+    """
+       This function implements the Postscript "pop operator". Calls self.opPop() to pop the top value from the opstack and discards the value.
+    """
+
+    def pop(self):
+        self.opPop()
+
+    """
+       Prints the opstack. The end of the list is the top of the stack.
+    """
+
+    def stack(self):
+        reversedList = self.opstack[::-1]
+        for x in reversedList:
+            print(x)
+    """
+       Copies the top element in opstack.
+    """
+
+    def dup(self):
+        op1 = self.opPop()
+        self.opPush(op1)
+        self.opPush(op1)
+
+    """
+       Pops an integer count from opstack, copies count number of values in the opstack.
+    """
+
+    def copy(self):
+        count = self.opPop()
+        count += 1
+        if len(self.opstack) >= count:
+            for i in range(count):
+                if(type(self.opstack[i]) is int):
+                    self.opPush(self.opstack[i])
+
+    """
+        Counts the number of elements in the opstack and pushes the count onto the top of the opstack.
+    """
+
+    def count(self):
+        count = len(self.opstack) - 1
+        self.opPush(count)
+
+    """
+       Clears the opstack.
+    """
+
+    def clear(self):
+        self.opstack[:] = []
+
+    """
+       swaps the top two elements in opstack
+    """
+
+    def exch(self):
+        if len(self.opstack) > 1:
+            op1 = self.opPop()
+            op2 = self.opPop()
+            self.opPush(op1)
+            self.opPush(op2)
+
+    """
+       Pops an integer from the opstack (size argument) and pushes an  empty dictionary onto the opstack.
+    """
+
+    def psDict(self):
+        self.opPop()
+        self.opPush({})
+
+    """
+       Pops the dictionary at the top of the opstack; pushes it to the dictstack.
+    """
+
+    def begin(self):
+        newDict = self.opPop()
+        if isinstance(newDict, dict):
+            self.dictPush(newDict)
+        else:
+            print("error, not of type dict")
+
+    """
+       Removes the top dictionary from dictstack.
+    """
+
+    def end(self):
+        self.dictPop()
+
+    """
+       Pops a name and a value from opstack, adds the name:value pair to the top dictionary by calling define.
+    """
+
+    def psDef(self):
+        if len(self.opstack) > 1:
+            value = self.opPop()
+            name = self.opPop()
+            self.define(name, value)
+        else:
+            print("error: op stack is too empty")
+
+    # ------- if/ifelse Operators --------------
+    """
+       Inplements if operator.
+       Pops the `ifbody` and the `condition` from opstack.
+       If the condition is True, evaluates the `ifbody`.
+    """
+
+    def psIf(self):
+        if len(self.opstack) > 1:
+            ifBody = self.opPop()
+            condition = self.opPop()
+            if condition == True:
+                ifBody.apply(self)
+            else:
+                self.opPush(condition)
+                self.opPush(ifBody)
+        # TO-DO in part2
+
+    """
+       Inplements ifelse operator.
+       Pops the `elsebody`, `ifbody`, and the condition from opstack.
+       If the condition is True, evaluate `ifbody`, otherwise evaluate `elsebody`.
+    """
+
+    def psIfelse(self):
+        if len(self.opstack) > 2:
+            elseBody = self.opPop()
+            ifBody = self.opPop()
+            condition = self.opPop()
+            if condition:
+                ifBody.apply(self)
+            else:
+                elseBody.apply(self)
+        else:
+            self.opPush(condition)
+            self.opPush(ifBody)
+            self.opPush(elseBody)
+
+        #TO-DO in part2
+
+    # ------- Loop Operators --------------
+    """
+       implements for operator.
+       Pops the `loopbody`, `end`index, `increment`, `start` index arguments from opstack;
+       loop counter starts at `start` , incremented by `increment` value, and ends at `end`.
+       for each value of loop counter, push the counter value on opstack, and  evaluate the `loopbody`.
+    """
+
+    def psFor(self):
+        loopbody = self.opPop()
+        end = self.opPop()
+        increment = self.opPop()
+        start = self.opPop()
+        if increment < 0:
+            for x in range(start, end-1, increment):
+                self.opPush(x)
+                loopbody.apply(self)
+                if (end-1 == x-1):
+                    self.opPush(x)
+                    loopbody.apply(self)
+
+        elif increment > 0:
+            for x in range(start, end+1, increment):
+                self.opPush(x)
+                loopbody.apply(self)
+
+            # TO-DO in part2
+
+    def clearBoth(self):
+        self.opstack[:] = []
+        self.dictstack[:] = []
+
+    def cleanTop(self):
+        if len(self.opstack) > 1:
+            if self.opstack[-1] is None:
+                self.opstack.pop()
